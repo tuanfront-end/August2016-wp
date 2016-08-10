78bb3126430fb0f8b6420de6d7ab8a936811973a
eden_tuan <atuanvptd@gmail.com>
2016-08-10 16:59:48 +0700

    #Search - #About - #Contact

--- search.php
+++ search.php
@@ -9,31 +9,46 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
+	<article>
+		<div class="entry-wrap search-rs">
+			<h1 class="page-title"><?php printf( esc_html__( 'Search results for %1$s   ', 'eden' ), '<strong><i class="fa fa-angle-double-left" aria-hidden="true"></i>' . get_search_query() . '<i class="fa fa-angle-double-right" aria-hidden="true"></i></strong>' );
+			 ?>
+			 </h1>
+			<?php 
+				$search_query = new WP_Query('s='.$s.'&showpost=-1');
+				$search_keyword = _wp_specialchars($s,1);
+				$search_count= $search_query->found_posts;
+				
+				printf(__('<h4>%1$s  Item Found </h4>','eden'),$search_count);
+			 ?>
+			
+		</div>
+	</article>
+
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		if ( have_posts() ) :
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'eden' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
 
 			<?php
+			endif;
+
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
-
-			the_posts_navigation();
-
+			?>
+			 <div class="page-num">
+			 	<?php echo the_posts_navigation2(); ?>
+			 </div>
+			<?php
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
@@ -41,8 +56,8 @@ get_header(); ?>
 		endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying results in search pages.
+ * Template part for displaying posts.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -10,27 +10,52 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('card'); ?>>
+	
+		<div class="entry-wrap "> 
 
-	<div class="row">
-		<div class="col-md-10 entry-wrap">
 			<header class="entry-header">
-				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-				<?php if ( 'post' === get_post_type() ) : ?>
+				<div class="post-tag">
+					<span>I Do Travel</span>
+				</div>
+				<?php 
+				if ( 'post' === get_post_type() ) : ?>
 				<div class="entry-meta">
 					<?php eden_posted_on(); ?>
 				</div><!-- .entry-meta -->
-				<?php endif; ?>
+				<?php
+				endif; ?>
+ 
+				<?php
+				if ( is_single() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
+				?>
+				<hr>
+
 			</header><!-- .entry-header -->
 
-			<div class="entry-summary">
-				<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
-
-			<footer class="entry-footer">
-				<?php eden_entry_footer(); ?>
-			</footer><!-- .entry-footer -->
+			<div class="entry-content">
+				
+				<?php
+					the_content( sprintf(
+						/* translators: %s: Name of current post. */
+						wp_kses( __( 'read more', 'eden' ), array( 'span' => array( 'class' => array() ) ) ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					) );
+				?>
+					
+					<footer class="entry-footer">
+						<?php eden_entry_footer(); ?>
+					</footer><!-- .entry-footer -->
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Page :', 'eden' ),
+						'after'  => '</div>'
+					) );
+				?>
+			</div><!-- .entry-content -->
 		</div>
-	</div>
-
+		
 </article><!-- #post-## -->
